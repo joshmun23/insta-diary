6a4c4a937a1e5157212b0257d91951fdca28e047
Joshua Mun <joshmun23@gmail.com>
2015-05-12 16:25:24 -0400

    add has_many schedules association to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   has_many :subscriptions
   has_many :plans, through: :subscriptions
+  has_many :schedules, through: :subscriptions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
